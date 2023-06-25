05ab35989a6cc824184c0a9f0d103c2af423e545
Julian Mair <github@julianmair.com>
2023-06-26 01:13:03 +0200

    First exercise done

---
+++ Basics/Exercise/2_variables/2_variables_exercise.py
@@ -0,0 +1,24 @@
+### Exercise: Python Variables
+#1. Create a variable called break and assign it a value 5. See what happens and find out the reason behind the behavior that you see.
+breakingPoint = 5
+#2. Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+birthYear = 1992
+currentYear = 2023
+age = currentYear - birthYear
+print('You are ' + str(int(age)) + ' years old')
+#3. Store your first, middle and last name in three different variables and then print your full name using these variables
+firstname = 'Julian '
+middlename = 'no middle name '
+lastname = 'Mair'
+
+print('My name is ' + firstname + middlename + lastname)
+#4. Answer which of these are invalid variable names: 
+#   _nation
+#   1record
+#   record1
+#   record_one
+#   record-one
+#   record^one
+#   continue
+   
+#   [Solution](https://github.com/codebasics/py/blob/master/Basics/Exercise/2_variables/2_variables_exercise.ipynb)
